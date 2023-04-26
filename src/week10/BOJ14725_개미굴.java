01d0688ba1dc2fbf479bc5a473209dfcdc00ae9b
jyj1143 <jyj1143@gmail.com>
2023-04-26 20:30:10 +0900

    개미굴

---
+++ src/week10/BOJ14725_개미굴.java
@@ -0,0 +1,64 @@
+package week10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+public class Main {
+
+	static TreeMap<String, TreeMap> root;
+	static int N;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		root = new TreeMap<String, TreeMap>();
+
+		N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int size = Integer.parseInt(st.nextToken());
+			TreeMap<String, TreeMap> cur = root;
+			for (int s = 0; s < size; s++) {
+				String str = st.nextToken().toString();
+
+				if (cur.get(str) == null) {
+					cur.put(str, new TreeMap<String, TreeMap>());
+				}
+
+				cur = cur.get(str);
+			}
+
+		}
+
+		for (String key : root.keySet()) {
+			System.out.println(key);
+			
+			DFS(1, root.get(key));
+		}
+
+		br.close();
+
+	}
+
+	static void DFS(int level, TreeMap<String, TreeMap> map) {
+		if (map == null) {
+			return;
+		} else {
+			for (String key : map.keySet()) {
+				for(int i=0;i<level;i++) {
+					System.out.print("--");
+				}
+				
+				System.out.println(key);
+				DFS(level+1, map.get(key));
+			}
+
+		}
+	}
+
+}
+
