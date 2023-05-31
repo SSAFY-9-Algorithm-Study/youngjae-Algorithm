ea9740a2addc96d771a0154966c03d183bceac72
jyj1143 <jyj1143@gmail.com>
2023-05-31 20:27:29 +0900

    숨바꼭질3

--- src/week13/BOJ13549_숨바꼭질3.java
+++ src/week13/BOJ13549_숨바꼭질3.java
@@ -38,9 +38,10 @@ public class BOJ13549_숨바꼭질3 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		start = Integer.parseInt(st.nextToken());
 		end = Integer.parseInt(st.nextToken());
-		dist = new int[200001];
+		dist = new int[200001]; // 동생은 점 K(0 ≤ K ≤ 100,000) 있을수 있다.
+		// 2배로 많이 멀리 보냈다가 점차 작아지는게 더 빨리 찾을 수 있어서, 200001로 잡음
 
-		if (end <= start) {
+		if (end <= start) { // 동생지점이 수빈이 지점보다 낮은 경우 -1로만 이동
 			answer = start - end;
 		} else {
 			answer = BFS(start);
@@ -50,18 +51,20 @@ public class BOJ13549_숨바꼭질3 {
 		br.close();
 	}
 
+	// 다익스트라
 	static int BFS(int start) {
 		PriorityQueue<Point> queue = new PriorityQueue();
 		queue.add(new Point(start, 0));
 
-		for (int i = 0; i < 200001; i++) {
+		for (int i = 0; i < 200001; i++) { // 거리 최대로 초기화
 			dist[i] = Integer.MAX_VALUE;
 		}
-
+		dist[start] = 0;
 		while (!queue.isEmpty()) {
 
 			Point cur = queue.poll();
 
+			// 더 짧은 거리인지 확인
 			if (cur.x + 1 <= 100000 && dist[cur.x + 1] > cur.level + 1) {
 				dist[cur.x + 1] = cur.level + 1;
 				queue.add(new Point(cur.x + 1, dist[cur.x + 1]));
